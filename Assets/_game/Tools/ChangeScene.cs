44f9b49e5ef684ab38094a3f22b94f325f3c8b82
Tuan Hung Mai <hung.mai2k0@gmail.com>
2023-10-24 23:13:25 +0700

    add photon + format

---
+++ Assets/_game/Tools/ChangeScene.cs
@@ -0,0 +1,38 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+
+public class ChangeScene : Editor {
+
+    [MenuItem("Open Scene/Loading #1")]
+    public static void OpenLoading()
+    {
+        OpenScene("Loading");
+    }
+
+    [MenuItem("Open Scene/Home #2")]
+    public static void OpenHome()
+    {
+        OpenScene("Home");
+    }
+    [MenuItem("Open Scene/HomeSnow #3")]
+    public static void OpenHomeSnow()
+    {
+        OpenScene("HomeSnow");
+    }
+    
+    [MenuItem("Open Scene/GamePlay #4")]
+    public static void OpenGamePlay()
+    {
+        OpenScene("GamePlay");
+    }
+
+    
+    private static void OpenScene (string sceneName) {
+		if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo ()) {
+			EditorSceneManager.OpenScene ("Assets/_game/Scenes/" + sceneName + ".unity");
+		}
+	}
+}
+#endif
